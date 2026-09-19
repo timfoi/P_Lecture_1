738ea8791c1466ac7592adae65ec3742ce0785a3
m0trex <timofeyalisov@gmail.com>
2026-09-19 20:28:00 +0300

    Data struct and adapter func

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
-#include <future>
+#include <exception>
 #include <iostream>
+#include <pthread.h>
 #include <random>
 #include <vector>
 
@@ -9,6 +10,20 @@ constexpr size_t THREADS_COUNT = 4;
 size_t calc(double r, size_t tests, size_t seed);
 double area(double r, size_t threads, size_t tests);
 bool isInside(double x, double y, double r);
+
+struct Data
+{
+  double r;
+  size_t tests, seed;
+  size_t res = 0;
+};
+
+void *adapter(void *data)
+{
+  Data *task = static_cast< Data * >(data);
+  task->res = calc(task->r, task->tests, task->seed);
+  return nullptr;
+}
 
 int main()
 {
@@ -46,19 +61,7 @@ size_t calc(double r, size_t tests, size_t seed)
 
 double area(double r, size_t threads, size_t tests)
 {
-  std::vector< std::future< size_t > > res;
-  res.reserve(threads);
-
-  for (size_t i = 0; i < threads; ++i) {
-    res.push_back(std::async(std::launch::async, calc, r, tests, i));
-  }
-
-  size_t count = 0;
-  for (size_t i = 0; i < threads; ++i) {
-    count += res[i].get();
-  }
-
-  return 4 * r * r * static_cast< double >(count) / static_cast< double >(threads * tests);
+  // return 4 * r * r * static_cast< double >(count) / static_cast< double >(threads * tests);
 }
 
 bool isInside(double x, double y, double r)
