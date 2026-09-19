28c0e6a59d57f85a6df65c94301b77bc51ef9c35
m0trex <timofeyalisov@gmail.com>
2026-09-19 19:40:03 +0300

    Error handling

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
+#include <cmath>
 #include <future>
 #include <iostream>
 #include <random>
 #include <vector>
+
+constexpr size_t THREADS_COUNT = 4;
 
 size_t calc(double r, size_t tests, size_t seed);
 double area(double r, size_t threads, size_t tests);
 bool isInside(double x, double y, double r);
 
 int main()
-{}
+{
+  double r = 0.0;
+  size_t tests = 0;
+  if (!(std::cin >> r >> tests) || r <= 0.0) {
+    std::cerr << "Invalid data" << '\n';
+    return 1;
+  }
+  try {
+    double res = area(r, THREADS_COUNT, tests);
+    std::cout << "Монте-Карло — " << res << '\n';
+    std::cout << "Формула — " << std::acos(-1.0) * r * r << '\n';
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+}
 
 size_t calc(double r, size_t tests, size_t seed)
 {
